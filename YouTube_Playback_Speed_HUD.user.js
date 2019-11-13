f2f6ae6aa891bfdfbf11fa63cae2c52e6945e7e3
Shane Burgess <acropup@users.noreply.github.com>
2019-11-13 08:35:04 -0800

    Update to match youtube's new site
    - Now taking advantage of getPlaybackRate() instead of searching the DOM for the selected playback rate
    - Handles YouTube's partial page reloads now, since YouTube doesn't do full page loads between pages anymore. Waits until the page is ready before making changes so that they stick to the player after navigation.
    - Added option to hide miniplayer and theater mode buttons

--- YouTube_Playback_Speed_HUD.user.js
+++ YouTube_Playback_Speed_HUD.user.js
@@ -1,106 +1,124 @@
 // ==UserScript==
 // @name         YouTube Playback Speed HUD
-// @version      0.1
+// @version      0.2
 // @description  Show YouTube playback speed and time of day next to the Settings icon
 // @homepage     https://github.com/acropup/acropup-Tampermonkey-Scripts/
 // @author       Shane Burgess
-// @match        https://www.youtube.com/watch*
-// @match        https://www.youtube.com/embed*
+// @match        https://www.youtube.com/*
+// @run-at       document-start
 // ==/UserScript==
 
-/**** Note! Playback speed HUD does not appear until you've changed ****
- **** the speed at least once on the current page.                  ****
- **** Playback speed keyboard shortcuts are Shift+> and Shift+<.    ****/
+/**** Playback speed keyboard shortcuts are Shift+> and Shift+<. ****/
 
-//*** EDIT THESE TO CUSTOMIZE THIS SCRIPT ***
-var HIDE_PLAY_ON_TV     = true;
+/*** EDIT THESE VARS TO CUSTOMIZE THIS SCRIPT ***/
 var SHOW_TIME_OF_DAY    = true;
 var SHOW_PLAYBACK_SPEED = true;
+var HIDE_PLAY_ON_TV_BTN = true; //Play on TV button is for sending to Chromecast
+var HIDE_MINIPLAYER_BTN = true;
+var HIDE_VIEW_SIZE_BTN  = false; //Toggle for Theater mode or Default view (keyboard shortcut 't' still works)
 
-(function() {
-    'use strict';
-    var right_control = document.getElementsByClassName("ytp-right-controls")[0];
-    
-    if (HIDE_PLAY_ON_TV) {
-        var chromecast_button = right_control.getElementsByClassName("ytp-remote-button")[0];
-        if (chromecast_button) {
-            chromecast_button.style.display = "none";
+
+'use strict';
+//Wait until video player is loaded before modifying HUD
+on_player_ready(customize_HUD);
+
+function customize_HUD() {
+    if (SHOW_TIME_OF_DAY)    { show_time_of_day(); }
+    if (SHOW_PLAYBACK_SPEED) { show_playback_speed(); }
+    if (HIDE_PLAY_ON_TV_BTN) { hide_HUD_item("ytp-remote-button"); }
+    if (HIDE_MINIPLAYER_BTN) { hide_HUD_item("ytp-miniplayer-button"); }
+    if (HIDE_VIEW_SIZE_BTN)  { hide_HUD_item("ytp-size-button"); }
+}
+
+var right_control; //placeholder for "ytp-right-controls" HUD container
+function get_right_control() {
+    right_control = right_control || document.getElementsByClassName("ytp-right-controls")[0];
+    return right_control;
+}
+
+function add_HUD_item(class_name) {
+    if (get_right_control()) {
+        var new_item = document.createElement("div");
+        new_item.className = class_name;
+        right_control.insertBefore(new_item, right_control.firstChild);
+        return new_item;
+    }
+}
+
+function hide_HUD_item(itemClassName) {
+    if (get_right_control()) {
+        var hud_item = right_control.getElementsByClassName(itemClassName)[0];
+        if (hud_item) {
+            hud_item.style.display = "none";
+        }
+    }
+}
+
+function show_time_of_day() {
+    //Add custom div to show current system time (useful when fullscreen)
+    var system_time = add_HUD_item("ytp-button");
+    if (system_time) {
+        function update_time() {
+            var d = (new Date()).toLocaleTimeString(); //Returns "HH:mm:ss AMPM"
+            var t = d.substring(0, d.lastIndexOf(":"));//Keep "HH:mm" part
+            if (system_time.innerText != t) {
+                system_time.innerText = t;
+            }
+            //Update 5ms after the next minute has started
+            var s = d.substr(d.lastIndexOf(":") + 1, 2);
+            var next_update = 1000*(60-s) + 5;
+            setTimeout(update_time, next_update);
+        }
+        update_time();
+    }
+}
+
+function show_playback_speed() {
+    //Add custom div to show current playback speed
+    var speed_HUD = add_HUD_item("ytp-button");
+    var playdiv = document.getElementsByClassName("html5-video-player")[0];
+    function update_playback_speed() {
+        var rate = playdiv.getPlaybackRate();
+        if (rate) speed_HUD.innerText = rate + "x";
+    };
+    update_playback_speed();
+    //Update speed_HUD every time there's a keypress or mouse click
+        //TODO: Could filter these events somewhat, to act only in
+        //      cases that might have affected playback speed.
+    document.onclick = document.onkeyup = update_playback_speed;
+}
+
+function missing_essential_elements() {
+    if (undefined === get_right_control()) return true;
+    //There is only one div of class "html5-video-player" per page/iframe.
+    //The ID of this div (normally #movie_player) is inconsistent for embedded videos.
+    var movie_player = document.getElementsByClassName("html5-video-player")[0];
+    if (undefined === movie_player) return true;
+    if (undefined === movie_player.getPlaybackRate) return true;
+    return false;
+}
+
+function on_player_ready(callback, retries = 4) {
+    //Execute callback once player is ready
+    //"retries" parameter gives the page's javascript time to finish up after document.readyState says it's complete.
+    if (missing_essential_elements()) {
+        if (document.readyState != "complete") {
+            setTimeout(()=>on_player_ready(callback), 250);
         }
         else {
-            console.log("Chromecast Button not found.");
+            //Document is apparently complete, but not all elements are ready.
+            //Give the page's javascript a little more time to finish up.
+            if (retries > 0) {
+                setTimeout(()=>on_player_ready(callback, retries-1), 250);
+            }
+            else {
+                //Abandon customization if not all elements exist once page is finished loading
+            }
         }
     }
-    
-    if (SHOW_TIME_OF_DAY) {
-        //Add custom div to show current system time (useful when fullscreen)
-        (function() {
-            var system_time = document.createElement("div");
-            system_time.className = "ytp-button";
-            right_control.insertBefore(system_time, right_control.firstChild);
-            var updateTime = function() {
-                var d = (new Date()).toLocaleTimeString(); //Returns "HH:mm:ss AMPM"
-                system_time.innerText = d.substring(0, d.lastIndexOf(":"));
-            };
-            updateTime();
-            //Update every minute
-            setInterval(updateTime, 60000);
-        })();
+    else {
+        //All elements are loaded and ready
+        callback();
     }
-    
-    if (SHOW_PLAYBACK_SPEED) {
-        //Add custom div to show current playback speed
-        var speed_HUD = document.createElement("div");
-        speed_HUD.className = "ytp-button";
-        right_control.insertBefore(speed_HUD, right_control.firstChild);
+}
 
-        /* Notes as of 24/06/2017:
-         - ytp-bezel element does not have aria-label attribute until 
-           playback speed is adjusted by keyboard shortcuts.
-         - ytp-bezel's aria-label attribute does not update when
-           playback speed is adjusted through the Settings menu.
-         - <div class="ytp-menuitem-label">Speed</div> does not exist 
-           until the Settings menu is accessed for the first time.
-         - If Speed menuitem exists, its value is always correct.
-        */
-
-        var speed_source_overlay;
-        var speed_source_menuitem;
-        var getPlaybackSpeed = function() {
-            //Check menuitem first because it is always correct (if it exists), whereas 
-            //the overlay's value doesn't update if speed is changed through the Settings menu.
-            if (!speed_source_menuitem) {
-                var settings = document.getElementsByClassName("ytp-menuitem-label");
-                for (var i = 0; i < settings.length; i++) {
-                    if (settings[i].innerText == "Speed") break;
-                }
-                if (i < settings.length) {
-                    speed_source_menuitem = settings[i].nextSibling;
-                }
-            }
-            if (speed_source_menuitem) {
-                var spd = speed_source_menuitem.innerText;
-                return (spd == "Normal") ? "1x" : spd + "x";
-            }
-            //Check overlay if user hasn't opened the Settings menu yet
-            if (!speed_source_overlay) {
-                speed_source_overlay = document.getElementsByClassName("ytp-bezel")[0];
-            }
-            if (speed_source_overlay) {
-                //speed_source_overlay has a label attribute of the form "Speed is 1.25"
-                var spd = speed_source_overlay.getAttribute('aria-label');
-                var match = /Speed is (.*)/.exec(spd);
-                if (match) return match[1] + 'x';
-            }
-            //If neither element is initialized yet, return nothing
-            return "";
-        };
-
-        //Update speed_HUD every time there's a keypress or mouse click
-        document.onclick = document.onkeydown = function(e) {
-            //TODO: Could filter these events somewhat, to act only in
-            //      cases that might have affected playback speed.
-            var new_speed = getPlaybackSpeed();
-            if (new_speed) speed_HUD.innerText = new_speed;
-        };
-    }
-})();
